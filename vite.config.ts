99804a7b78d0fa1f45a27ec0c447dc96becb578d
chnbsdan <chnbsdan@gmail.com>
2025-08-31 14:08:38 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   base: './',
   root: resolve('src/renderer'),
   build: {
-    outDir: resolve(__dirname, 'dist'),  // 输出到根目录
+    outDir: resolve(__dirname, 'src/renderer/dist'), // 输出到 src/renderer/dist
     emptyOutDir: true
   },
   resolve: {
